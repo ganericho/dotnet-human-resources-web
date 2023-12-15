920918f5bc2297925b5b5b8963e491cb878ce9b5
Henricho Dirganeri <131332450+rikoedr@users.noreply.github.com>
2023-12-16 07:39:31 +0800

    refactor(api) : rename RepositoryResult class to RepositoryHandler

---
+++ api/Utilities/Handlers/RepositoryHandler.cs
@@ -0,0 +1,8 @@
+﻿namespace api.Utilities.Handlers;
+
+public class RepositoryHandler<T>
+{
+    public bool IsFailed { get; set; } = false;
+    public string? Exception { get; set; } = MessageCollection.NoException;
+    public T? Result { get; set; }
+}
--- api/Utilities/RepositoryResult.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace api.Utilities;
-
-public class RepositoryResult <T>
-{
-    public bool IsFailed { get; set; }
-    public string? Status { get; set; }
-    public T? Data { get; set; }
-}
