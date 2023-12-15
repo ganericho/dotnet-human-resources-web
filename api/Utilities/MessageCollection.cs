d7d617b79ec6d55b41a8ebc435cacbb74653e87a
Henricho Dirganeri <131332450+rikoedr@users.noreply.github.com>
2023-12-16 07:40:20 +0800

    feat(api) : add HashingHandler and MessageCollection in utilities

---
+++ api/Utilities/MessageCollection.cs
@@ -0,0 +1,14 @@
+﻿namespace api.Utilities;
+
+public class MessageCollection
+{
+    // Repository Message
+    public static string NoException = "No Exception";
+    public static string Empty = "Empty";
+    public static string DataCreated = "Data Created";
+    public static string DataUpdated = "Data Updated";
+    public static string DataDeleted = "Data Deleted";
+    public static string DataFound = "Data Found";
+    public static string DataNotFound = "Data Not Found";
+    public static string NoResult = "No Result Data";
+}
